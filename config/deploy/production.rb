5dc8e9c6fb08d874a271b6594f7cbfa9e99c5167
Jeremy Dahan <dahnmills@gmail.com>
2015-10-11 22:39:58 +0200

    change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
-set :stage, :production
+# set :stage, :production
 
-role :app, %w{deployer@192.168.1.14}
-role :web, %w{deployer@192.168.1.14}
-role :db,  %w{deployer@192.168.1.14}
+# role :app, %w{deployer@192.168.1.14}
+# role :web, %w{deployer@192.168.1.14}
+# role :db,  %w{deployer@192.168.1.14}
 
-server '192.168.1.14', user: 'deployer', roles: %w{web app}
+# server '192.168.1.14', user: 'deployer', roles: %w{web app}
 
 # server-based syntax
 # ======================
@@ -49,21 +49,21 @@ server '192.168.1.14', user: 'deployer', roles: %w{web app}
 #
 # Global options
 # --------------
- set :ssh_options, {
-   keys: %w{/c/Users/Jeremy/.ssh/id_rsa},
-   forward_agent: false,
-   auth_methods: %w(password)
- }
+ # set :ssh_options, {
+ #   keys: %w{/c/Users/Jeremy/.ssh/id_rsa},
+ #   forward_agent: false,
+ #   auth_methods: %w(password)
+ # }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+server '192.168.1.14',
+  user: 'deployer',
+  roles: %w{web app},
+  ssh_options: {
+    user: 'deployer', # overrides user setting above
+    keys: %w{/c/Users/Jeremy/.ssh/id_rsa},
+    forward_agent: false,
+    auth_methods: %w(publickey password)
+    # password: 'please use keys'
+  }
