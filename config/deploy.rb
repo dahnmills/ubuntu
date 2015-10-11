44c02a76566d27dc22d3e09c0038bc2471060ee1
Jeremy Dahan <dahnmills@gmail.com>
2015-10-11 22:45:47 +0200

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@bitbucket.org:dahnmills/ubuntu.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deployer/ubuntu'
+# set :deploy_to, '/home/deployer/ubuntu'
 
 # Default value for :scm is :git
 # set :scm, :git
