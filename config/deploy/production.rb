a36496c63093d7b408fecdc67de96e123e445fc9
Jeremy Dahan <dahnmills@gmail.com>
2015-10-12 02:06:31 +0200

    github 1st

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -57,11 +57,11 @@
 # The server-based syntax can be used to override options:
 # ------------------------------------
 server '192.168.1.14',
-  user: 'dahn',
+  user: 'deployer',
   roles: %w{web app db},
   use_sudo: false,
   ssh_options: {
-    user: 'dahn', # overrides user setting above
+    user: 'deployer', # overrides user setting above
     keys: %w{/c/Users/Jeremy/.ssh/id_rsa},
     forward_agent: true,
     paranoid: true,
