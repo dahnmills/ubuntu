8e18332912cd79c8daff6d4a35c46e5e35f4c13d
Jeremy Dahan <dahnmills@gmail.com>
2015-10-12 21:50:16 +0200

    change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -57,7 +57,7 @@
 # The server-based syntax can be used to override options:
 set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
 # ------------------------------------
-server '192.168.1.14',
+server '192.168.1.43',
   user: 'deployer',
   roles: %w{web app db},
   use_sudo: false,
